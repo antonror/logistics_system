b8fb7d22bbf7901e3e6823bc521b4b355c746aae
antonror <hitty@i.ua>
2020-06-21 14:18:03 +0300

    TDD: failing spec ahead of the first line of code

---
+++ spec/services/moteefe_service_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe MoteefeService do
+  context 'when service is preset' do
+    it 'should be present' do
+      expect { desribed_class }.to be_present
+    end
+  end
+end
