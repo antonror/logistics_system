7fa25e2565c46dbd26546cd5608a85fc83116864
antonror <hitty@i.ua>
2020-06-21 22:11:16 +0300

    TDD: Scenario 1-3 implementation

--- app/services/moteefe_service.rb
+++ app/services/moteefe_service.rb
@@ -2,7 +2,10 @@
 class MoteefeService < ApplicationService
   def initialize(*args)
     @args = args
-
+    @order_data = {
+      delivery_date: Date.today,
+      shipments: []
+    }
   end
 
   def call
@@ -10,23 +13,119 @@ class MoteefeService < ApplicationService
   end
 
   def proceed
-
+    single_or_multiple_item_order
+    return @order_data
   end
 
   private
 
-  def blank_result
-    {
-        date: '',
-        shipments: {
-            supplier: '',
+  def single_or_multiple_item_order
+    if @args.length == 1
+      process_single_item_order
+    else
+      process_multiple_item_order
+    end
+  end
+
+  def process_single_item_order
+    item = @args[0].keys[0]
+    qty = @args[0].values[0]
+    suppliers = SupplierStock.where('product_name=? AND in_stock >= ?', item, qty)
+
+    unless suppliers.empty?
+      final_supplier = suppliers.sort_by{ |supplier| supplier.delivery_times["uk"] }.first
+      delivery_date =  Date.today + final_supplier.delivery_times["uk"].days
+      @order_data = {
+        delivery_date: delivery_date,
+        shipments: [
+          {
+            supplier: final_supplier.supplier,
+            delivery_date: delivery_date,
             items: [
-                {
-                    title: '',
-                    count: 0
-                }
+              {
+                title: item, count: qty
+              }
             ]
+          }
+        ]
+      }
+    else
+      all_partial_suppliers     = SupplierStock.where('product_name=? AND in_stock >= 1', item)
+      sorted_partial_suppliers  = all_partial_suppliers.sort_by{ |supplier| supplier.delivery_times["uk"] }
+      first_partial_supplier    = sorted_partial_suppliers.first
+      rest_of_qty               = qty - first_partial_supplier.in_stock
+      next_partial_supplier     = sorted_partial_suppliers.second
+      qty_fulfilled             =  (rest_of_qty - next_partial_supplier.in_stock) <= 0 ? true : false
+
+      if qty_fulfilled
+        @order_data = {
+          delivery_date: Date.today + next_partial_supplier.delivery_times["uk"].days,
+          shipments: [
+            {
+              supplier: first_partial_supplier.supplier,
+              delivery_date: Date.today + first_partial_supplier.delivery_times["uk"].days,
+              items: [
+                {
+                  title: item, count: first_partial_supplier.in_stock
+                }
+              ]
+            },
+            {
+              supplier: next_partial_supplier.supplier,
+              delivery_date: Date.today + next_partial_supplier.delivery_times["uk"].days,
+              items: [
+                {
+                  title: item, count: rest_of_qty
+                }
+              ]
+            }
+          ]
         }
+      else
+        'I CAN NOT COMBINE FROM MORE THAN 2 SUPPLIERS YET'
+      end
+    end
+
+  end
+
+  def process_multiple_item_order
+    suppliers_for_comparison = []
+
+    @args.each do |order_line|
+      item = order_line.keys[0]
+      qty = order_line.values[0]
+      suppliers_for_comparison << SupplierStock.where('product_name=? AND in_stock >= ?', item, qty).map(&:supplier)
+    end
+
+    unless suppliers_for_comparison.length > 2
+      final_supplier_name = (suppliers_for_comparison.first & suppliers_for_comparison.last)[0]
+      @args.each do |order_line|
+        item = order_line.keys[0]
+        qty = order_line.values[0]
+        order = SupplierStock.where('supplier=? AND product_name=?', final_supplier_name, item)[0]
+        delivery_date = Date.today + order.delivery_times["uk"].days
+        @order_data[:shipments] << { supplier: order.supplier, items: [{ title: order.product_name, count: qty }] }
+        @order_data[:delivery_date] = delivery_date if @order_data[:delivery_date] < delivery_date
+      end
+    else
+      'I CAN NOT BUY MORE THAN 2 DIFFERENT ITEMS FROM SINGLE SUPPLIER YET'
+    end
+  end
+
+
+  def blank_result
+    @order_data = {
+      delivery_date: '',
+      shipments: {
+        supplier: '',
+        delivery_date: '',
+        items: [
+          {
+              title: '',
+              count: 0
+          }
+        ]
+      }
     }
   end
 end
--- spec/services/moteefe_service_spec.rb
+++ spec/services/moteefe_service_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe MoteefeService do
   before :each do
     create(:supplier_stock, supplier: 'SupplierA', product_name: 't-shirt', delivery_times: { uk: 3, us: 8 }, in_stock: 6 )
     create(:supplier_stock, supplier: 'SupplierB', product_name: 't-shirt', delivery_times: { uk: 1, us: 8 }, in_stock: 7 )
-    create(:supplier_stock, supplier: 'SupplierB', product_name: 'hoodie1', delivery_times: { uk: 3, us: 8 }, in_stock: 4 )
-    create(:supplier_stock, supplier: 'SupplierC', product_name: 'hoodie1', delivery_times: { uk: 5, us: 8 }, in_stock: 7 )
+    create(:supplier_stock, supplier: 'SupplierB', product_name: 'hoodie', delivery_times: { uk: 3, us: 8 }, in_stock: 4 )
+    create(:supplier_stock, supplier: 'SupplierC', product_name: 'hoodie', delivery_times: { uk: 5, us: 8 }, in_stock: 7 )
   end
 
   describe 'class hierarchy' do
@@ -17,20 +17,21 @@ RSpec.describe MoteefeService do
     end
   end
 
-  describe 'output generation'
+  describe 'output generation' do
     context 'with no argument' do
       it 'should provide output without given arguments' do
         output = {
-            date: '',
-            shipments: {
-                supplier: '',
-                items: [
-                    {
-                        title: '',
-                        count: 0
-                    }
-                ]
-            }
+          delivery_date: '',
+          shipments: {
+            supplier: '',
+            delivery_date: '',
+            items: [
+              {
+                  title: '',
+                  count: 0
+              }
+            ]
+          }
         }
 
         result = MoteefeService.new().call
@@ -38,4 +39,36 @@ RSpec.describe MoteefeService do
         expect(result).to eq(output)
       end
     end
+
+    context 'with valid arguments' do
+
+      # 7 t-shirts in stock as a maximum from 1 supplier
+      # we take 8 to satisfy condition 1
+      it 'should provide valid result for scenario 1' do
+        result = MoteefeService.new({ 't-shirt' => 8 }).call
+        expect(result[:delivery_date]).to eq(Date.today + 3.days)
+        expect(result[:shipments].count).to eq(2)
+        expect(result[:shipments][0][:items][0][:count]).to eq(7)
+        expect(result[:shipments][1][:items][0][:count]).to eq(1)
+      end
+
+      # We select the fastest supplier having requested amount
+      it 'should provide valid result for scenario 2' do
+        result = MoteefeService.new({ 't-shirt' => 5 }).call
+        expect(result[:delivery_date]).to eq(Date.today + 1.days)
+        expect(result[:shipments].count).to eq(1)
+        expect(result[:shipments][0][:items][0][:count]).to eq(5)
+      end
+
+      # We select single supplier with requested goods
+      # for multi-line order
+      it 'should provide valid result for scenario 3' do
+        result = MoteefeService.new({ 't-shirt' => 1 }, {'hoodie' => 1}).call
+        expect(result[:delivery_date]).to eq(Date.today + 3.days)
+        expect(result[:shipments].count).to eq(2)
+        expect(result[:shipments][0][:items][0][:title]).to eq('t-shirt')
+        expect(result[:shipments][1][:items][0][:title]).to eq('hoodie')
+      end
+    end
+  end
 end
